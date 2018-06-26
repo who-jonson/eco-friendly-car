c96427a24b5ee102f0506f67b21eb441f79a18cb
Jonson <19268881+who-jonson@users.noreply.github.com>
2018-06-27 02:51:56 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('SITE_TITLE', 'Eco-Friendly Car');
+define('ROOT_URL', 'http://localhost/eco-friendly-car/');
+
+//DB_PARAMS
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "eco_friendly_car");
